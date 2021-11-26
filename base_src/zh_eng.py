a577f063264dfdae77695b02004eb635ef37431a
Robbie Selwyn <robbieselwyn@gmail.com>
2021-11-26 02:01:08 -0800

    AB tree search added for baseline

--- base_src/zh_eng.py
+++ base_src/zh_eng.py
@@ -12,14 +12,37 @@ piece_values = {
 	chess.KING: 1000
 }
 
+LARGE_NUM = 100000
+
+DESIRED_DEPTH = 7 # ?
+
+# By making the piece more value in the "bag," the piece implicitly doesn't have as high of an incentive to be dropped.
+# intuition is that pawns/bishops
+droppable_scalar = {
+	chess.PAWN: .5,
+	chess.KNIGHT: .5,
+	chess.BISHOP: .4,
+	chess.ROOK: 1,
+	chess.QUEEN: 1,
+}
+
+
 ALL_PIECES = [chess.PAWN, chess.KNIGHT, chess.BISHOP, chess.ROOK, chess.KING, chess.QUEEN]
 
 def evaluate_board(board, color):
 	eval_score_curr_player = 100 # Total Eval Score for the current position.  Start at 100 just to ensure > 0
 
+	if board.is_checkmate():
+		return 1000000 # For all intents and purposes +inf
+
+	# Raw Material
 	for piece in ALL_PIECES:
 		for piece_instance in board.pieces(piece, color):
 			eval_score_curr_player += piece_values[piece]
+
+	# Material Held in Pocket
+	for droppable_piece in board.pockets[color]: # All the droppable pieces for the current player
+		eval_score_curr_player += piece_values[droppable_piece] * droppable_scalar[droppable_piece]
 
 	return eval_score_curr_player
 
@@ -37,6 +60,71 @@ def generate_legal_zh_moves(board):
 			moves.append(chess.Move(from_square=sq, to_square=sq, drop=droppable_piece)) # Add those as move options
 
 	return moves
+
+def best_move(board):
+	alpha = -1000000;
+    beta = 1000000;
+    tp_table = {} # We don't need to evaluate the same board twice.  Essentially memoization for a chess engine
+
+    best_move = None
+    best_move_score = -100000
+
+    for move in generate_legal_zh_moves(board):
+		score = perform_tree_search_(board, DESIRED_DEPTH, True, alpha, beta, tp_table)
+		"""
+		            // Update Alpha and Beta
+            if (!b->sideToMove) {
+                alpha = std::max(alpha, extreme_value[index]);
+            }
+            else {
+                beta = std::min(beta, extreme_value[index]);
+            }"""
+
+        if score > best_move_score:
+        	best_move = move
+        	best_move_score = score
+
+    return (best_move, best_move_score)
+
+# Performs AB minimax search given a depth, player, a, b, and transposition table
+# Based on https://github.com/rselwyn/tactic-generator/blob/master/src/engine.cpp#L167
+def perform_tree_search_(board, depth, isMaximizing, alpha, beta, tp_table):
+	if depth == 0:
+		return evaluate_(board)
+
+	if board.fen() in tp_table:
+		return tp_table[board.fen()] # memoization
+
+	if isMaximizing:
+		bestValue = - LARGE_NUM
+		possibleMoves = []
+		for candidate_move in generate_legal_zh_moves(board):
+			board.push(candidate_move)
+			minimax_result = perform_tree_search_(board, depth - 1, not isMaximizing, alpha, beta, tp_table)
+			bestValue = max(bestValue, minimax_result)
+			alpha = max(alpha, bestValue)
+			board.pop()
+
+			if alpha > beta:
+				break
+
+		tp_table[board.fen()] = bestValue
+		return bestValue
+	else:
+		bestValue = LARGE_NUM
+		possibleMoves = []
+		for candidate_move in generate_legal_zh_moves(board):
+			board.push(candidate_move)
+			minimax_result = perform_tree_search_(board, depth - 1, not isMaximizing, alpha, beta, tp_table)
+			bestValue = min(bestValue, minimax_result)
+			beta = min(beta, bestValue)
+			board.pop()
+
+			if beta < alpha:
+				break
+
+		tp_table[board.fen()] = bestValue
+		return bestValue
 
 
 zh_board = chess.variant.CrazyhouseBoard()

0f97ae16d0f09cc09f6fc66df73e09a6fa7a93e3
Abhay Singhal <abhayaaditsinghal@gmail.com>
2021-12-05 14:53:08 -0800

    trained linear regression and ElasticNet

---
+++ q_learning/model/linear_regression/lin_reg_model.py
@@ -0,0 +1,41 @@
+from sklearn.decomposition import TruncatedSVD
+from sklearn.linear_model import ElasticNet, LinearRegression
+import pickle
+from os import path
+import sys
+sys.path.append(r"..\..")
+import util
+import tqdm
+import numpy as np
+
+DATA_DIR = r"..\..\..\..\data\bughouse-db"
+
+filename = "export2018"
+file = path.join(DATA_DIR, "bpgn", f"{filename}.bpgn")
+filepath = path.join(DATA_DIR, "pk", f"{filename}.pk")
+
+print("Loading dataset")
+with open(filepath, 'rb') as f:
+    data_dict = pickle.load(f)
+
+print("Processing dataset")
+states, values = util.dataset_to_array(data_dict, tqdm.tqdm, max_n=2000)
+
+print("Fitting linear regression")
+reg = LinearRegression().fit(states, values)
+
+preds = reg.predict(states)
+print(f"Predictions: min = {np.min(preds)}, max = {np.max(preds)}, mean = {np.mean(preds)}, mean diff from mean: {np.mean(preds - np.mean(preds))}")
+score = reg.score(states, values)
+rmse = np.sqrt(np.mean((preds - values) ** 2))
+print(f"Model: score = {score}, RMSE = {rmse}")
+
+print("Fitting ElasticNet")
+reg = ElasticNet().fit(states, values)
+
+preds = reg.predict(states)
+print(f"Predictions: min = {np.min(preds)}, max = {np.max(preds)}, mean = {np.mean(preds)}, mean diff from mean: {np.mean(preds - np.mean(preds))}")
+score = reg.score(states, values)
+rmse = np.sqrt(np.mean((preds - values) ** 2))
+print(f"Model: score = {score}, RMSE = {rmse}")
+
--- q_learning/util.py
+++ q_learning/util.py
@@ -1,4 +1,5 @@
 import numpy as np
+import tqdm
 
 
 def hash_state_to_int(state):
@@ -31,3 +32,27 @@ def hash_state(state):
 
 def unhash_state(hashed):
     return unhash_state_from_string(hashed)
+
+
+def dataset_to_array(data_dict, tqdm, max_n=None):
+    states = []
+    values = []
+    iter_max = len(data_dict.keys())
+    if max_n < iter_max:
+        iter_max = max_n
+
+    with tqdm(total=iter_max) as pbar:
+        for i, (state_hash, value) in enumerate(data_dict.items()):
+            state = unhash_state(state_hash)
+            if state.shape[0] != 3840:
+                pbar.update(1)
+                continue
+            states.append(state)
+            values.append(np.mean(value))
+            pbar.update(1)
+            if i >= iter_max-1:
+                break
+    assert len(states) == len(values)
+    states = np.stack(states)
+    values = np.stack(values)
+    return states, values
